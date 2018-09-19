ea22eba2e2e6db7e31e3be9677ec0fe4d5267c65
tn_mai <rabitfiver@gmail.com>
2018-09-20 04:50:08 +0900

    タブを2スペースに変換.
    テキストは紙面の都合で2スペースインデントを採用しているため、合わせておくほうがコピーが捗る.

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -7,17 +7,17 @@
 /// エントリーポイント.
 int main()
 {
-	GLFWEW::Window& window = GLFWEW::Window::Instance();
-	if (!window.Init(800, 600, "OpenGL Tutorial")) {
-		return 1;
-	}
+  GLFWEW::Window& window = GLFWEW::Window::Instance();
+  if (!window.Init(800, 600, "OpenGL Tutorial")) {
+    return 1;
+  }
 
-	// メインループ.
-	while (!window.ShouldClose()) {
-		glClearColor(0.1f, 0.3f, 0.5f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		window.SwapBuffers();
-	}
+  // メインループ.
+  while (!window.ShouldClose()) {
+    glClearColor(0.1f, 0.3f, 0.5f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    window.SwapBuffers();
+  }
 
-	return 0;
+  return 0;
 }
