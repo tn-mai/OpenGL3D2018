26360d4c7a68bf00a5884e8acf9532734eb7da54
tn_mai <rabitfiver@gmail.com>
2018-12-06 15:55:59 +0900

    メインゲームシーンの雛形を作成.

---
+++ Src/MainGameScene.cpp
@@ -0,0 +1,40 @@
+/**
+* @file MainGameScene.cpp
+*/
+#include "MainGameScene.h"
+
+/**
+* ‰Šú‰».
+*/
+bool MainGameScene::Initialize()
+{
+  return true;
+}
+
+/**
+* “ü—Í”½‰f.
+*/
+void MainGameScene::ProcessInput()
+{
+}
+
+/**
+* ó‘ÔXV.
+*/
+void MainGameScene::Update()
+{
+}
+
+/**
+* •`‰æ.
+*/
+void MainGameScene::Render() const
+{
+}
+
+/**
+* I—¹.
+*/
+void MainGameScene::Finalize()
+{
+}
---
+++ Src/MainGameScene.h
@@ -0,0 +1,40 @@
+/**
+* @file MainGameScene.h
+*/
+#ifndef MAINGAMESCENE_H_INCLUDED
+#define MAINGAMESCENE_H_INCLUDED
+#include "GLFWEW.h"
+#include "Texture.h"
+#include "Shader.h"
+#include "MeshList.h"
+
+/**
+* メインゲームシーン.
+*/
+class MainGameScene
+{
+public:
+  MainGameScene() = default;
+  ~MainGameScene() = default;
+
+  bool Initialize();
+  void ProcessInput();
+  void Update();
+  void Render() const;
+  void Finalize();
+
+private:
+  MeshList meshList;
+
+  Texture::Image2D texId;
+  Texture::Image2D texTree;
+  Texture::Image2D texHouse;
+  Texture::Image2D texRock;
+  Texture::Image2D texHuman;
+
+  Shader::Program progSimple;
+  Shader::Program progLighting;
+  Shader::LightList lightList;
+};
+
+#endif // MAINGAMESCENE_H_INCLUDED
