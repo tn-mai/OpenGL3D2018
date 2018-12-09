0c45721fc2651c70787b1111d09d0943d1537cae
田中 真依 <rabitfiver@gmail.com>
2018-12-10 04:39:56 +0900

    シーン切り替えのための機能を追加.

--- Src/Scene.h
+++ Src/Scene.h
@@ -3,6 +3,7 @@
 */
 #ifndef SCENE_H_INCLUDED
 #define SCENE_H_INCLUDED
+#include <string>
 
 /**
 * シーンの基底クラス.
@@ -18,6 +19,12 @@ public:
   virtual void Update() = 0;
   virtual void Render() = 0;
   virtual void Finalize() = 0;
+
+  const std::string& NextScene() const { return nextScene; }
+  void NextScene(const char* name) { nextScene = name; }
+
+private:
+  std::string nextScene;
 };
 
 #endif // SCENE_H_INCLUDED
